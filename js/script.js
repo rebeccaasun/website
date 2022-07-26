15c44969d36d14034ecd2e73d7e1d73c4bf351ec
Rebecca Sun <rebeccasun4@gmail.com>
2022-07-26 18:14:58 -0400

    modified colors and css

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById("github-button").addEventListener("click", visitGithub);
+document.getElementById("linkedin-button").addEventListener("click", visitLinkedIn);
+
+function visitGithub() {
+    window.location.assign("https://github.com/rebeccaasun");
+}
+
+function visitLinkedIn() {
+    window.location.assign("https://www.linkedin.com/in/rebeccaasun/");
+}
